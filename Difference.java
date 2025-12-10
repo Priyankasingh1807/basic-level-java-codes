2b87929490a3121fc3906ea8687238143867e055
Priyanka Singh <personaluse.priyanka@gmail.com>
2025-12-10 23:37:30 +0530

    Initial commit

---
+++ Difference.java
@@ -0,0 +1,9 @@
+public class Difference {
+    public static void main(String[] args) {
+        int a = 10;
+        int b = 5;
+        int difference = (b - a);
+        System.out.println(difference);
+    }
+
+}
