5529f587306a8a9deb179d9c5997e7f9f49ea921
Priyanka Singh <personaluse.priyanka@gmail.com>
2025-12-11 16:40:43 +0530

    add new java program

---
+++ Largest.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+import java.util.*;
+
+public class Largest {
+    public static void main(String args[]) {
+        Scanner sc = new Scanner(System.in);
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+        int c = sc.nextInt();
+        int max = a;
+        if (max < b) {
+            max = b;
+        }
+        if (max < c) {
+            max = c;
+        }
+        System.out.println("Largest number is: " + max);
+
+        {
+            sc.close();
+        }
+    }
+}
