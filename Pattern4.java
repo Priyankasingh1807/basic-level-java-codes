98159138cfc1976ba705badf75412dd9fd67e58f
Priyanka Singh <personaluse.priyanka@gmail.com>
2025-12-12 20:26:32 +0530

    pattern program

---
+++ Pattern4.java
@@ -0,0 +1,10 @@
+public class Pattern4 {
+    public static void main(String args[]) {
+        int n = 4;
+        for (int i = n; i >= 1; i--) {
+
+            System.out.println();
+        }
+    }
+
+}
