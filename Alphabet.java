db5d5a22441f210885f04cac05e0adb696cb4ff8
Priyanka Singh <personaluse.priyanka@gmail.com>
2025-12-12 10:35:04 +0530

    add

---
+++ Alphabet.java
@@ -0,0 +1,19 @@
+import java.util.*;
+
+public class Alphabet {
+    public static void main(String args[]) {
+        Scanner sc = new Scanner(System.in);
+        char ch = sc.next().charAt(0);
+        if (ch >= 65 && ch <= 90) {
+            System.out.println("Uppercase alphabet");
+        }
+        if (ch >= 97 && ch <= 122) {
+            System.out.println("Lowercase alphabet");
+        }
+
+        {
+            sc.close();
+        }
+    }
+
+}
---
+++ Division.java
@@ -0,0 +1,9 @@
+public class Division {
+    public static void main(String args[]){
+        int a=4;
+        int b=34;
+        int division=(b/a);
+        System.out.println(division);
+    }
+    
+}
