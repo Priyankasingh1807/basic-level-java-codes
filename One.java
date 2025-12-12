a5912065c19a0d85b386c6b49d25b578c7bf67bf
Priyanka Singh <personaluse.priyanka@gmail.com>
2025-12-12 21:36:22 +0530

    pattern

--- One.java
+++ One.java
@@ -1,9 +1,9 @@
-public class Pattern4 {
+public class One {
     public static void main(String args[]) {
-        int n = 4;
-        for (int i = n; i >= 1; i--) {
+        int n = 5;
+        for (int i = 1; i <= n; i++) {
             for (int j = 1; j <= i; j++) {
-                System.out.print("*");
+                System.out.print(j + " ");
             }
             System.out.println();
         }
--- One2.java
+++ One2.java
@@ -1,9 +1,9 @@
-public class Pattern4 {
-    public static void main(String args[]) {
-        int n = 4;
+public class One2 {
+    public static void main(String[] args) {
+        int n = 5;
         for (int i = n; i >= 1; i--) {
             for (int j = 1; j <= i; j++) {
-                System.out.print("*");
+                System.out.print(j + " ");
             }
             System.out.println();
         }
